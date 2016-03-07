da9b91cae3c015d949ae356169f34cec3e683160
nathancw <nathanielcw@gmail.com>
2016-03-07 15:16:52 -0600

    Dont make fun of my ugly code and comments. I think it is all wrong but we will see when i ask him :(

--- Crypto/src/Client.java
+++ Crypto/src/Client.java
@@ -1,15 +1,26 @@
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.Date;
 
 public class Client {
 
 	Socket serverSocket;
 	String timeStamp;
+	String pubKey;
+	String privKey;
 	
 	public static void main(String args[])
     {
@@ -21,18 +32,69 @@ public class Client {
 	public Client(){
 		
 		try {
+			//THIS IS ALICE SHE IS A BEAUTIFUL CLIENT
+			//Generate Some keys first
+			 Key alicePublicKey = null;
+	         KeyPairGenerator kpg;
+				try {
+					System.out.println("Generating RSA Shared Key inside Alice  " + System.currentTimeMillis());
+					kpg = KeyPairGenerator.getInstance("RSA");
+					kpg.initialize(1024);
+					KeyPair kp = kpg.genKeyPair();
+					alicePublicKey = kp.getPublic();
+					Key alicePrivateKey = kp.getPrivate();
+					System.out.println("Key Generation Done for Alice.  " + System.currentTimeMillis());
+					
+				} catch (NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+			//Connect to server
 			serverSocket = new Socket("localhost",9090);
-			System.out.println("Connected to localhost in port 9090");
+			System.out.println("Connected to localhost in port 9090  " + System.currentTimeMillis());
 			
-			 DataOutputStream dos = new DataOutputStream(serverSocket.getOutputStream());
+			 //DataOutputStream dos = new DataOutputStream(serverSocket.getOutputStream());
+			 ObjectOutputStream keyOut = new ObjectOutputStream(serverSocket.getOutputStream());
 			
-			 byte[] message = new byte[3];
+			 byte[] message = new byte[2000];
 			
-			 message[0] = 'a';
-			 message[1] = 5;
-	         dos.writeInt(message.length);
-	         dos.write(message);
-	         dos.flush();
+			 //Prepare the message
+			 for(int i = 0; i < 2000; i++)
+				 message[i] = 'a';
+		
+			 
+	
+			 
+	        //From Alice to bob we need to use RSA. So we need a shared secret key.
+			//Send publicKey to Bob
+			//dos.writeInt(aliceByteKey.length);
+			System.out.println("Sending Public Key to Bob  " + System.currentTimeMillis());
+			keyOut.writeObject(alicePublicKey);
+				
+				//dos.writeInt(message.length);
+			    //dos.write(message);
+			 
+			    
+			    
+			    
+			    
+			    
+			    
+	         //Reading in from the server
+	         DataInputStream dIn = new DataInputStream(serverSocket.getInputStream());
+	         
+	         byte[] serverMessage;
+	         int length = dIn.readInt();           // read length of incoming message
+	         message = new byte[length];
+	         dIn.readFully(message, 0, message.length); // read the message
+	  
+	         String m = new String(message);
+	         System.out.println(m);
+	         
+	         
+	         
+	         
 			 //dos.close();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
--- Crypto/src/Server.java
+++ Crypto/src/Server.java
@@ -1,9 +1,15 @@
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 
 public class Server {
 	Socket clientSocket = null;
@@ -14,24 +20,57 @@ public class Server {
 	
 	public Server(){
 		try {
+			//THIS IS BOB HE IS A BEAUTIFUL SERVER
 			ServerSocket connection = new ServerSocket(9090);
-			System.out.println("Waiting for connection.");
+			System.out.println("Waiting for connection." + System.currentTimeMillis());
             clientSocket = connection.accept();
-            System.out.println("Connection received from " + connection.getInetAddress().getHostName());
+            System.out.println("Connection received from " + connection.getInetAddress().getHostName() + " " +System.currentTimeMillis());
         
-            DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
-         
+            //DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
             
-           int length = dIn.readInt();                    // read length of incoming message
-            //if(length>0) {
-                //byte[] message = new byte[length];
-                //dIn.readFully(message, 0, message.length); // read the message
-                
-            //}
-           byte[] message = new byte[3];
-           dIn.read(message);
-         System.out.println(message[1]);
+            //Read in public key from Alice
+            ObjectInputStream keyIn = new ObjectInputStream(clientSocket.getInputStream());
+            Key alicePubKey = null;
+            try {
+            	System.out.println("Trying to read in Alice Pub Keyy. -----"+ System.currentTimeMillis());
+				alicePubKey = (Key) keyIn.readObject();
+				System.out.println("Read in Alice's Public Key: " + alicePubKey + "-----"+ System.currentTimeMillis());
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
+            //Now to generate the encryption using Alice's Public Key
+            
+            
+           //Create and prepare the message we want to send.
+            DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
 			
+			byte[] clientMessage = new byte[2000];
+			
+			for(int i = 0; i < 2000; i++)
+				clientMessage[i] = 'b';
+			////
+			
+			 //So from bob to alice he wants us to use So they need to agree on a shared secret key.
+
+			dos.writeInt(clientMessage.length);
+	        dos.write(clientMessage);
+                
+	       
+                
+            
+          /*
+           *   byte[] message;
+            int length = dIn.readInt(); 
+            message = new byte[length];
+            dIn.readFully(message, 0, message.length); // read the message
+  
+            String m = new String(message);
+            System.out.println(m);
+           * 
+           */
+      	
           // dIn.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
