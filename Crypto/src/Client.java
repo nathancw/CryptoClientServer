5aead8e7daacf496af199cfb9d9b62a7f653b43e
nathancw <nathanielcw@gmail.com>
2016-03-16 15:21:32 -0500

    Included 100% more secret key sharing using RSA

--- Crypto/src/Client.java
+++ Crypto/src/Client.java
@@ -1,62 +1,150 @@
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Date;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 public class Client {
 
 	Socket serverSocket;
 	String timeStamp;
-	String pubKey;
+	String alicepubKey;
 	String privKey;
+	public static PublicKey alicePublicKey;
+	PrivateKey alicePrivateKey;
+	PublicKey bobPublicKey;
+	SecretKey atobSecretKey;
 	
 	public static void main(String args[])
     {
-        Client client = new Client();
- 
+        //Create connection and the keys
+		Client client = new Client();
+		byte[] secretKey;
+		byte [] encryptedText = null;
+        
+		
+		try {
+			Cipher RSAencrKey = Cipher.getInstance("RSA/ECB/NoPadding");
+			try {
+				//Try to encrypt the key
+				RSAencrKey.init(Cipher.ENCRYPT_MODE, client.getBobPublicKey());
+				secretKey = client.getatobSecretKey().getEncoded();
+				try {
+					//Do the encryption.
+					encryptedText = RSAencrKey.doFinal(secretKey);
+				} catch (IllegalBlockSizeException e) {
+					e.printStackTrace();
+				} catch (BadPaddingException e) {
+					e.printStackTrace();
+				}
+			} catch (InvalidKeyException e) {
+				e.printStackTrace();
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		}
+        
+		System.out.println("EncryptedText:" + encryptedText);
+		
+		DataInputStream dIn;
+		DataOutputStream dos;
+		//Try to write the encrypted text to the server
+		try {
+			System.out.println("Sending text to bob: " + Arrays.toString(encryptedText));
+			dos = new DataOutputStream(client.getServerSocket().getOutputStream());
+			dIn = new DataInputStream(client.getServerSocket().getInputStream());
+			dos.writeInt(encryptedText.length);
+			dos.write(encryptedText);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 	
 	
+	private Socket getServerSocket() {
+		return serverSocket;
+	}
+
+
+	private SecretKey getatobSecretKey() {
+		return atobSecretKey;
+	}
+
+
+	private PublicKey getBobPublicKey() {
+		return bobPublicKey;
+	}
+
+
 	public Client(){
 		
 		try {
 			//THIS IS ALICE SHE IS A BEAUTIFUL CLIENT
-			//Generate Some keys first
-			 Key alicePublicKey = null;
+			
+			 alicePublicKey = null;
 	         KeyPairGenerator kpg;
+	              
 				try {
-					System.out.println("Generating RSA Shared Key inside Alice  " + System.currentTimeMillis());
+					//Generate Pub/Priv keys using RSA below
+					System.out.println("Generating RSA Shared Key inside Alice.");
 					kpg = KeyPairGenerator.getInstance("RSA");
 					kpg.initialize(1024);
 					KeyPair kp = kpg.genKeyPair();
 					alicePublicKey = kp.getPublic();
-					Key alicePrivateKey = kp.getPrivate();
-					System.out.println("Key Generation Done for Alice.  " + System.currentTimeMillis());
+					alicePrivateKey = kp.getPrivate();
+					System.out.println("Key Generation Done for Alice.");
+					////////////////
 					
+					//Generate Secret Key to be sent to Bob
+					KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+				    keyGen.init(128);
+				    atobSecretKey = keyGen.generateKey();
+				    System.out.println("Generated Secret Key in Alice: " + Arrays.toString(atobSecretKey.getEncoded()));
+		
 				} catch (NoSuchAlgorithmException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			
+				
+			//READ BOBS PUBLIC KEY IN
+			readBobsKey();
+				
 			//Connect to server
 			serverSocket = new Socket("localhost",9090);
 			System.out.println("Connected to localhost in port 9090  " + System.currentTimeMillis());
-			
-			 //DataOutputStream dos = new DataOutputStream(serverSocket.getOutputStream());
-			 ObjectOutputStream keyOut = new ObjectOutputStream(serverSocket.getOutputStream());
-			
+
 			 byte[] message = new byte[2000];
 			
 			 //Prepare the message
@@ -64,35 +152,7 @@ public class Client {
 				 message[i] = 'a';
 		
 			 
-	
-			 
-	        //From Alice to bob we need to use RSA. So we need a shared secret key.
-			//Send publicKey to Bob
-			//dos.writeInt(aliceByteKey.length);
-			System.out.println("Sending Public Key to Bob  " + System.currentTimeMillis());
-			keyOut.writeObject(alicePublicKey);
-				
-				//dos.writeInt(message.length);
-			    //dos.write(message);
-			 
-			    
-			    
-			    
-			    
-			    
-			    
-	         //Reading in from the server
-	         DataInputStream dIn = new DataInputStream(serverSocket.getInputStream());
-	         
-	         byte[] serverMessage;
-	         int length = dIn.readInt();           // read length of incoming message
-	         message = new byte[length];
-	         dIn.readFully(message, 0, message.length); // read the message
-	  
-	         String m = new String(message);
-	         System.out.println(m);
-	         
-	         
+	 
 	         
 	         
 			 //dos.close();
@@ -105,6 +165,30 @@ public class Client {
 		}
 		
 		
+		
+		
+	}
+	
+	public void readBobsKey() throws IOException{
+		
+		FileInputStream keyfis = new FileInputStream("BobPublicKey");
+		byte[] encKey = new byte[keyfis.available()];  
+		keyfis.read(encKey);
+		
+		X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
+		KeyFactory keyFactory;
+		try {
+			keyFactory = KeyFactory.getInstance("RSA");
+			bobPublicKey = keyFactory.generatePublic(pubKeySpec);
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 		
 		
 	}
--- Crypto/src/Server.java
+++ Crypto/src/Server.java
@@ -1,81 +1,157 @@
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class Server {
 	Socket clientSocket = null;
+	public static PublicKey bobPublicKey;
+	PublicKey alicePubKey = Client.alicePublicKey;
+	PrivateKey bobPrivateKey;
+	
 	public static void main(String args[])
     {
         Server server = new Server();
     }
 	
 	public Server(){
-		try {
-			//THIS IS BOB HE IS A BEAUTIFUL SERVER
-			ServerSocket connection = new ServerSocket(9090);
-			System.out.println("Waiting for connection." + System.currentTimeMillis());
-            clientSocket = connection.accept();
-            System.out.println("Connection received from " + connection.getInetAddress().getHostName() + " " +System.currentTimeMillis());
-        
-            //DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
-            
-            //Read in public key from Alice
-            ObjectInputStream keyIn = new ObjectInputStream(clientSocket.getInputStream());
-            Key alicePubKey = null;
-            try {
-            	System.out.println("Trying to read in Alice Pub Keyy. -----"+ System.currentTimeMillis());
-				alicePubKey = (Key) keyIn.readObject();
-				System.out.println("Read in Alice's Public Key: " + alicePubKey + "-----"+ System.currentTimeMillis());
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
+		//THIS IS BOB HE IS A BEAUTIFUL SERVER
+		//Set up connection
+		
+		KeyPairGenerator kpg;
+	
+	
+		try{
+			
+		//Generate Pub/Priv keys using RSA below
+		System.out.println("Generating RSA Shared Key inside Bob/Server  " + System.currentTimeMillis());
+		kpg = KeyPairGenerator.getInstance("RSA");
+		kpg.initialize(1024);
+		KeyPair kp = kpg.genKeyPair();
+		bobPublicKey = kp.getPublic();
+		bobPrivateKey = kp.getPrivate();
+		//System.out.println("Key Generation Done for Bob/Server. BobPublicKey:" + bobPublicKey + System.currentTimeMillis());
+		////////////////
+		
+		//Put Public key into encoded bytes and store in file for alice to grab
+		byte[] bytePublicKey = bobPublicKey.getEncoded();
+
+			try {
+				FileOutputStream keyfos = new FileOutputStream("BobPublicKey");
+				keyfos.write(bytePublicKey);
+				keyfos.close();
+			} catch (IOException e) {
+			
 				e.printStackTrace();
 			}
-            
-            //Now to generate the encryption using Alice's Public Key
-            
-            
-           //Create and prepare the message we want to send.
-            DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+		
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+		
+		ServerSocket connection;
+		try {
+			//set up connection and then wait
+			connection = new ServerSocket(9090);
+			System.out.println("Waiting for connection." + System.currentTimeMillis());
+			//Wait for connection
+	        clientSocket = connection.accept();
+	        System.out.println("Connection received from " + connection.getInetAddress().getHostName() + " " +System.currentTimeMillis());
+	        /////////////////////////////
 			
-			byte[] clientMessage = new byte[2000];
-			
-			for(int i = 0; i < 2000; i++)
-				clientMessage[i] = 'b';
-			////
-			
-			 //So from bob to alice he wants us to use So they need to agree on a shared secret key.
-
-			dos.writeInt(clientMessage.length);
-	        dos.write(clientMessage);
-                
-	       
-                
-            
-          /*
-           *   byte[] message;
-            int length = dIn.readInt(); 
-            message = new byte[length];
-            dIn.readFully(message, 0, message.length); // read the message
-  
-            String m = new String(message);
-            System.out.println(m);
-           * 
-           */
-      	
-          // dIn.close();
+	        //System.out.println("Trying to read in Alice Pub Keyy2.: " + Client.alicePublicKey + "-----"+ System.currentTimeMillis());
+	        //Create and prepare the message we want to send.
+	        // DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+	 		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+        
+      
+        DataInputStream dIn;
+		try {
+			
+			dIn = new DataInputStream(clientSocket.getInputStream());
+			byte[] message;
+	        int length = dIn.readInt(); 
+	        message = new byte[length];
+	        dIn.readFully(message); 
+	  
+	        
+	        
+	        System.out.println("Received Text from Alice: " + Arrays.toString(message));
+	        
+	        //Decrypt the text
+	        byte[] decryptedText;
+	    	Cipher RSAdecrypt = Cipher.getInstance("RSA/ECB/NoPadding");
+			RSAdecrypt.init(Cipher.DECRYPT_MODE, bobPrivateKey);
+			decryptedText = RSAdecrypt.doFinal(message);
+	       
+	        System.out.println("Got this decrypted secret key from alice? : " + Arrays.toString(decryptedText));
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	        //String m = new String(message);
+	        //System.out.println(m);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      
+        
+            /*
+	 	byte[] clientMessage = new byte[2000];
+	 		
+	 		for(int i = 0; i < 2000; i++)
+	 			clientMessage[i] = 'b';
+ 
+
+ 		dos.writeInt(clientMessage.length);
+        dos.write(clientMessage);
+        */
+           
+
 			
 	}
 }
